e52320c21bb937d6d18a4635cfa0531b554bc70a
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2015-12-15 01:53:13 -0200

    fix(coverage): Setup codecov.io
    Report sourcing's code coverage to codecov.io.
    
    NOTE: The `type` fix doesn't really reflect a fix… I could not find
    another way of releasing this module's first version.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+exports.Entity           = require('./entity');
+exports.Event            = require('./event');
+exports.Report           = require('./report');
+exports.CommandHandler   = require('./command.handler');
+exports.DomainRepository = require('./domain.repository');
+exports.EventBus         = require('./event.bus');
